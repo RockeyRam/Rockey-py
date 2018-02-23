c9fb9ad405a6dada88b7ae6302a6157ad0910e16
RockeyRam <rockeyramsaran@gmail.com>
2018-02-23 12:33:03 +0530

    Add files via upload

---
+++ Beginner/MaximumOfArrayIndex.py
@@ -0,0 +1,15 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      Administrator
+#
+# Created:     23-02-2018
+# Copyright:   (c) Administrator 2018
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+a=input("Enter Integer")
+aint=str(a)
+c=map(int,str(aint))
+print("The Maximum Num is "+str(max(sorted(c))))
+
