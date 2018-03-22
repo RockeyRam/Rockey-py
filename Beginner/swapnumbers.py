70c22106340d799630dc28c916d740d5a4431dac
RockeyRam <rockeyramsaran@gmail.com>
2018-03-22 21:32:34 +0530

    Update swapnumbers.py

--- Beginner/swapnumbers.py
+++ Beginner/swapnumbers.py
@@ -1,1 +1,9 @@
-
+def main():
+  a=input("Enter Value:")
+  b=input("Enter Second Value")
+  print("The Before Swapping values are "+str(a)+" "+str(b))
+  t=a
+  a=b
+  b=t
+  print("The Swapping values are "+str(a)+" "+str(b))
+main()
