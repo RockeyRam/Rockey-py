402f2e051f2426af0d4bbe4bccdbd8d050657f89
RockeyRam <rockeyramsaran@gmail.com>
2018-03-29 14:45:36 +0530

    Create AdditionofnumOddorEven.py

---
+++ Beginner/AdditionofnumOddorEven.py
@@ -0,0 +1,9 @@
+def main():
+  n1=int(input("Enter Number"))
+  n2=int(input("Enter Number"))
+  add=n1+n2
+  if add%2==0:
+    print("The Number is Even")
+  else:
+    print("The Number is Odd")
+main()
