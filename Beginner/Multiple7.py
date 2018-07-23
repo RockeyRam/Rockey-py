07f975abab4b0c3ec8a1881067fd1068a2aec052
RockeyRam <rockeyramsaran@gmail.com>
2018-07-23 11:38:09 +0530

    Add files via upload

---
+++ Beginner/Multiple7.py
@@ -0,0 +1,7 @@
+def Multiple7(Value):
+    if(Value % 7 ==0):
+        print("It is Multiple of 7")
+    else:
+        print("It is not Multiple of 7")
+Num=int(input("Enter the Number"))
+Multiple7(Num)
