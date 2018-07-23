903eb807076f05edce56a9d71cfe026077551c85
RockeyRam <rockeyramsaran@gmail.com>
2018-07-23 11:32:53 +0530

    Update Prime.py

--- Beginner/Prime.py
+++ Beginner/Prime.py
@@ -1,12 +1,12 @@
 def Prime():
-    Num=int(input("Enter number"))
-    if Num>1:
-       for i in range(2,Num):
-            if(Num%i==0):
+    Num=int(input("Enter the Number"))
+    if Num > 1:
+        for i in range(2,Num):
+            if(Num % i==0):
                 print("It is not a prime number")
                 break
             else:
                 print("It is prime number")
-    else:
-        print("It is not a prime number")
+    else:  
+       print(num,"is not a prime number")  
 Prime()
