957acedfc2b6451a80486a6b9dd931b426a7b8f2
RockeyRam <rockeyramsaran@gmail.com>
2018-07-24 09:39:56 +0530

    Add files via upload

---
+++ Beginner/FindDiffandOddEven.py
@@ -0,0 +1,10 @@
+def DiffAndODDEVEN():
+    Num1=int(input("Enter the Number"))
+    Num2=int(input())
+    Diff=abs(Num1-Num2)
+    if (Num1+Num2)%2==0:
+        print("Even")
+    else:
+        print("Else")
+    
+DiffAndODDEVEN()
